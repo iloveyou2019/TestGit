c2261a6ec8fc460128944d94fa036af15996290d
rose <roseabc@163.com>
2019-03-13 14:39:36 +0800

    add mycode

---
+++ TestGit/src/main/java/com/ibm/zhang/TestGit/App.java
@@ -0,0 +1,13 @@
+package com.ibm.zhang.TestGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TestGit/src/main/java/com/ibm/zhang/TestGit/Test.java
@@ -0,0 +1,10 @@
+package com.ibm.zhang.TestGit;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("11111111111111");
+	}
+
+}
